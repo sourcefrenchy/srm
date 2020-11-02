1bf2f45ee52d53bf02bd3da886a92372c6a030be
JMA <jma@arrakis.local>
2020-11-02 14:42:55 -0500

    tuning, easier to read

--- srm.go
+++ srm.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"encoding/hex"
 	"flag"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"os"
@@ -17,65 +16,58 @@ import (
 // DEBUG to be set to enable verbose comments
 const DEBUG = false
 
-func delFile(file string, fileSize int64, random1 []byte, random2 []byte) {
-	f, err := os.OpenFile(file, os.O_WRONLY, 0666)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if DEBUG {
-		fmt.Printf("\n[*] Deleting all %d bytes from file %s:", fileSize, file)
-	}
-
-	defer println("File securely deleted.")
-
-	// Create a buffered writer from the file
-	bufferedWriter := bufio.NewWriter(f)
-	// Write bytes to buffer
-	bytesWritten, _ := bufferedWriter.Write(
-		random1,
-	)
-	if DEBUG {
-		fmt.Printf("\n==> Pass1: random %d bytes written (hash:%x)", bytesWritten, blake2b.Sum256(random1))
-	}
-	f.Close()
-
-	f, err = os.OpenFile(file, os.O_WRONLY, 0666)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Create a buffered writer from the file
-	bufferedWriter = bufio.NewWriter(f)
-	// Write bytes to buffer
-	bytesWritten, _ = bufferedWriter.Write(
-		random2,
-	)
-	if DEBUG {
-		fmt.Printf("\n==> Pass2: random %d bytes written (hash:%x)", bytesWritten, blake2b.Sum256(random2))
-	}
-	f.Close()
-
+func getHash(dat []byte) string {
 	hasher, _ := blake2b.New256(nil)
-	f, err = os.Open(file)
+	hasher.Write(dat)
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func delFile(file string, fileSize int64, random1 []byte, random2 []byte) {
+
+	if !DEBUG {
+		defer println("\nFile securely deleted.")
+	}
+
+	// ********* PASS1 WRITE - NO CHECK
+	err := ioutil.WriteFile(file, random1, 0)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer f.Close()
 
-	if _, err := io.Copy(hasher, f); err != nil {
-		log.Fatal(err)
-	}
-
-	hash := hasher.Sum(nil)
-	encodedHex := hex.EncodeToString(hash[:])
 	if DEBUG {
-		fmt.Printf("\n==> Pass3: verified matching pass2 (hash:%s)", encodedHex)
+		fmt.Printf("\n==> Pass1: random bytes overwritten: %x...", getHash(random1)[0:10])
 	}
-	f.Close()
-
-	err = os.Remove(file)
+	b, err := ioutil.ReadFile(file) // b has type []byte
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
+	}
+	// ********* PASS2 WRITE - NO CHECK
+	err = ioutil.WriteFile(file, random2, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if DEBUG {
+		fmt.Printf("\n==> Pass2: random bytes overwritten: %x...", getHash(random2)[0:10])
+	}
+
+	// ********* PASS3 CHECK PASS2
+	b, err = ioutil.ReadFile(file) // b has type []byte
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if DEBUG {
+		fmt.Printf("\n==> Pass3: verification:\n\tpass 2 hash: %x... <==>\n\tCurrent file content hash: %x...)", getHash(random2)[0:10], getHash(b)[0:10])
+	}
+
+	if DEBUG {
+		fmt.Println("\n==> file still on the filesystem (DEBUG mode on)")
+	} else {
+		err = os.Remove(file)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 	}
 }
 
